191f7a0019afca003b6d7cd427655026d5918546
NotParas <72342554+NotParas@users.noreply.github.com>
2023-01-29 17:19:03 +0530

    Update program1.c
    all comments now line up

--- program1.c
+++ program1.c
@@ -10,12 +10,12 @@ PROGRAM TO PRINT THE FOLLOWING PATTER:
 
 int main(){
 	int n = 5;
-	for(int i = 0; i < n; i++){ 			// outer for loop controls each line
-		for(int j = 0; j <= i; j++){ 		// inner for loop controls each character
+	for(int i = 0; i < n; i++){ 					// outer for loop controls each line
+		for(int j = 0; j <= i; j++){ 				// inner for loop controls each character
 			printf("*"); 					// will print i stars on the same line
 		}
 		printf("\n"); 						// go to the next line
 	}
 
-	return 0; 								// exit code
+	return 0; 							// exit code
 }
