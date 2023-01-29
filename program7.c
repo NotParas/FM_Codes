23ef72f17c03568ed3e18d9f3e4ad543ee924465
NotParas <72342554+NotParas@users.noreply.github.com>
2023-01-29 17:26:45 +0530

    Update program7.c
    all comments now line up

--- program7.c
+++ program7.c
@@ -5,22 +5,22 @@ PROGRAM TO CHECK IF A AN INPUT STRING IS PALINDROME OR NOT
 
 int main(){
 	char string[100];
-	printf("Enter a string: ");			// message for the user
-	scanf("%s", string);				// no & needed because 'string' is the default pointer of character array string[]
+	printf("Enter a string: ");				// message for the user
+	scanf("%s", string);					// no & needed because 'string' is the default pointer of character array string[]
 
 	int len = 0;						// find the length of the string
 	while(string[len] != '\0'){
 		len++;
 	}
 
-	int isPalindrome = 1; 				// flag (default set to 1)
+	int isPalindrome = 1; 					// flag (default set to 1)
 	for(int i = 0, j = len-1; i < j; i++, j--){
 		if(string[i] != string[j]){		
 			isPalindrome = 0;			// flag set to 0
 		}
 	}
-
-	if(isPalindrome){
+		
+	if(isPalindrome){					// check flag and accordingly give output
 		printf("String is a palindrome\n");
 	}else{
 		printf("String is not a palindrome\n");
