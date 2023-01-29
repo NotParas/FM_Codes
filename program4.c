7ee8c579a2da59cf4fbd355d85592caa74191ac0
NotParas <72342554+NotParas@users.noreply.github.com>
2023-01-29 17:22:57 +0530

    Update program4.c
    all comments now line up

--- program4.c
+++ program4.c
@@ -2,14 +2,14 @@
 PROGRAM TO TAKE STRING AS INPUT AND COUNT THE NUMBER OF VOWELS IN IT
 */
 #include <stdio.h>
-#include <string.h> 					// import for gets() function
+#include <string.h> 						// import for gets() function
 
 int main(){
 	char string[100];					// string is an array of characters
 	printf("Enter a sentence: ");
 	gets(string);
 
-	int i = 0;							// variable to loop through the string
+	int i = 0;						// variable to loop through the string
 	int vowelCount = 0;					// variable to store number of vowels
 	while(string[i] != '\0'){
 		switch(string[i]){
@@ -30,7 +30,7 @@ int main(){
 		since none of the cases have 'break',
 		line 22 will be executed for all cases 
 		*/
-		i++;							// increase i to go to the next char
+		i++;						// increase i to go to the next char
 	}
 	printf("Vowels: %d", vowelCount);
 
