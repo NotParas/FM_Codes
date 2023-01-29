f511bc7ca80d7849f22c6f629aa946416feadbd6
NotParas <72342554+NotParas@users.noreply.github.com>
2023-01-29 17:29:33 +0530

    Update program9.c
    added a few extra comments and all comments now line up

--- program9.c
+++ program9.c
@@ -6,20 +6,20 @@ PROGRAM TO TAKE AN ARRAY FROM USER AND PRINT ITS SIZE
 int main(){
 	int arr[100];
 	int n = 0;
-	char choice;
+	char choice;							// to count and store number of user inputs
 	
 	do{
 		printf("Enter element: ");
-		scanf("%d", &arr[n]);			// take user input and store it in array
-		n++; 							// count number of user inputs
+		scanf("%d", &arr[n]);					// take user input and store it in array
+		n++; 							// increase the count
 		printf("Enter 'Y' to enter next element\nOr anything else to quit: ");
-		scanf(" %c", &choice);			// ask user to continue or quit
+		scanf(" %c", &choice);					// ask user to continue or quit
 	}while((choice == 'y' || choice == 'Y') && n < 100);
 
 	printf("You entered %d elemnts in the array: ", n);
-										// print size of the array
+									// print size of the array
 	for(int i = 0; i < n; i++){
-		printf("%d ",arr[i]);			// print the array
+		printf("%d ",arr[i]);					// print the array
 	}
 
 	return 0;
